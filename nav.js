57dacd1cfa1edbcb042a84f35dcd48f2bbf7ff74
Janice Bader <janice.bader@hotmail.com>
2024-05-19 12:53:38 +0200

    first commit to github

---
+++ nav.js
@@ -0,0 +1,8 @@
+
+
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+    document.querySelector('.nav-links').style.display = 
+    (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+ });
+
+ 
